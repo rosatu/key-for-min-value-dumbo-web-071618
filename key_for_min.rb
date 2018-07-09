46632cfd6819bae04af63b564207361df7fd9793
Rosa Tu <rosalina.tu@gmail.com>
2018-07-09 21:10:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,16 @@
 
 def key_for_min_value(name_hash)
     value_array = []
+    sorted_values = []
     name_hash.each do |name,value|
       value_array.push(value)
     end
     value_array.each do |a,b|
-      b <=> a
+     if a < b
+       sorted_values.push(a)
     end  
       name_hash.each do |name,value|
-        if value_array[0] == value
+        if sorted_values[0] == value
         return name
         elsif name_hash == {}
         return nil
